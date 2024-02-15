92c0f1e3ff42b2fe42bbfc392a2da5b579adbbb1
YashashreePatel <yashashreepatel@gmail.com>
2024-02-15 17:41:10 -0500

    updating files formationg

--- backend.tf
+++ backend.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "gcs" {
-    bucket = "webapp-cicd-tf-state"
-    prefix = "static.tfstate.d"
+    bucket                      = "webapp-cicd-tf-state"
+    prefix                      = "static.tfstate.d"
     impersonate_service_account = "sa-webapp-tf-cicd@yash-cloud.iam.gserviceaccount.com"
   }
 }
--- terraform.tfvars
+++ terraform.tfvars
@@ -1,4 +1,4 @@
-project_id = "yash-cloud"
-region = "us-east1"
-zone = "us-east1-b"
+project_id         = "yash-cloud"
+region             = "us-east1"
+zone               = "us-east1-b"
 tf_service_account = "sa-webapp-tf-cicd@yash-cloud.iam.gserviceaccount.com"
