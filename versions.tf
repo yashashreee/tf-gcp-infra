4d2806bd3f00d521095f6ff3cdcadaa14fb51ed3
YashashreePatel <yashashreepatel@gmail.com>
2024-02-15 17:33:37 -0500

    updating readme

--- versions.tf
+++ versions.tf
@@ -1,10 +1,8 @@
-terraform {
-  required_version = "~>0.14.0"
-
-  required_providers {
-    google = {
-      source  = "hashicorp/google"
-      version = "~>3.64.0"
-    }
-  }
-}
+# terraform {
+#   required_providers {
+#     google = {
+#       source  = "hashicorp/google"
+#       version = "~>3.64.0"
+#     }
+#   }
+# }
