fd5563e6b280535a1b254b70cc8fa1b78ffcaedf
YashashreePatel <yashashreepatel@gmail.com>
2024-02-15 17:58:19 -0500

    adding service account key path

--- providers.tf
+++ providers.tf
@@ -1,4 +1,5 @@
 provider "google" {
+  credentials                 = file(var.gcp_key_path)
   project                     = var.project_id
   region                      = var.region
   zone                        = var.zone
