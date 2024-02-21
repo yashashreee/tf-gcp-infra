54007b43128c9232d69c836e0472fb429cced834
YashashreePatel <yashashreepatel@gmail.com>
2024-02-21 14:26:19 -0500

    adding VM instance and firewall rule

--- variables.tf
+++ variables.tf
@@ -4,14 +4,17 @@ variable "project_id" {
 
 variable "region" {
   description = "The Google Cloud region."
+  default     = "us-east1"
 }
 
 variable "routing_mode" {
   description = "VPC routing mode."
+  default     = "REGIONAL"
 }
 
 variable "zone" {
   description = "The Google Cloud zone."
+  default     = "us-east1"
 }
 
 variable "tf_service_account" {
@@ -24,12 +27,31 @@ variable "gcp_key_path" {
 
 variable "network_name" {
   description = "VPC network name for respective environment."
+  default     = "dev-netowrk"
 }
 
 variable "webapp_address" {
   description = "Routing address for webapp subnet."
+  default     = "10.0.1.0/24"
 }
 
 variable "db_address" {
   description = "Routing address for db subnet."
+  default     = "10.0.2.0/24"
+}
+
+variable "route_name" {
+  default = "webapp-route"
+}
+
+variable "instance_name" {
+  default = "webapp-vpc-instance"
+}
+
+variable "machine_type" {
+  default = "n2-standard-2"
+}
+
+variable "firewall" {
+  default = "webapp-firewall"
 }
